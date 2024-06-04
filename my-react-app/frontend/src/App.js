025a3c981cabf9ed8b42b8312158a4199dba5840
RaffsAccount <ryan.aziz.rafferty@hotmail.com>
2024-06-04 17:00:05 +1000

    Meat logo

--- my-react-app/frontend/src/App.js
+++ my-react-app/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from './Componenets/Login';
+import logo from './Assets/Our_Meat_Logo.png'; // Adjust the path based on your actual file location
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <img src={logo} alt="Company Logo" className="company-logo" />
                 <h1>Welcome to RaffCo's Stock Page</h1>
                 <Login />
             </header>
